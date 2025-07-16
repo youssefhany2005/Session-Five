8bbed5afdbacc3376c3d86e801022cc501bb0cf7
youssefhany2005 <yossefhany0@gmail.com>
2025-07-16 14:20:59 +0300

    V-01

--- Session Five/Program.cs
+++ Session Five/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Session_Five
+﻿using System.Globalization;
+
+namespace Session_Five
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Looping | Iteration Statements
+            //V-01
+            #region for - foreach
+            int[] Numbers = { 1, 2, 3, 4, 5 };
+
+            //Console.WriteLine($"Number 0 is {Numbers[0]}");
+            //Console.WriteLine($"Number 1 is {Numbers[1]}");
+            //Console.WriteLine($"Number 2 is {Numbers[2]}");
+            //Console.WriteLine($"Number 3 is {Numbers[3]}");
+            //Console.WriteLine($"Number 4 is {Numbers[4]}");
+
+
+            //for(int i=0;i<Numbers.Length;i++)
+            //{
+            //if(int==2)
+            //Continue
+            //break;
+           // Numbers[i] += 10;
+            //    Console.WriteLine($"Number {i} is {Numbers[i]}");
+            //}
+
+            //foreach (int item in Numbers)
+            //{
+            //    //item += 10;
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #endregion
         }
     }
 }
